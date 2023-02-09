951271c703542a74ac34cac6a1543e8f135946f2
NickOsipov <nick.osipov.91@gmail.com>
2023-02-10 01:38:14 +0400

    add structure directories

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # RecSysPost
+## Project in development process. Please don't clone repository!
+
+## How to start application
+
+### Building docker image
+`docker build -t recsys .`
+
+### Run container from building image
+`docker run --rm -d --name recsys_app -p 8000:8000 recsys`
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='src',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Recommendation system for posts with text data.',
+    license='MIT',
+    author='Nick Osipov',
+    author_email='nick.osipov.91@gmail.com',
+)
