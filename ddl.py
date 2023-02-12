ddf73147b24d85e8ef6552245bbf10d30c11a120
NickOsipov <nick.osipov.91@gmail.com>
2023-02-12 17:35:22 +0400

    edit ddl.py

--- ddl.py
+++ ddl.py
@@ -1,19 +1,19 @@
+import yaml
 import pandas as pd
-from sqlalchemy import create_engine
+
+# from database import ENGINE
 
 
-CONNECTION = "poastgres://postgres:postgres@root:postgres/db"
-CON = create_engine(CONNECTION)
-path_data_dict = {
-    "post_info": path_post_info,
-    "user_info": path_user_info,
-    "feed_data": path_feed_data,
-    "like_post": path_like_post
-}
+with open("config/config.yaml") as config_file:
+    config = yaml.safe_load(config_file)
+    config_data = config.get("DATA_PATH")
+
+print(config_data)
 
 
 if __name__ == "__main__":
 
-    for table_name, path in path_data_dict.items():
-        df = pd.read_parquet(path)
-        df.to_sql(table_name, con=CON)
+    for table_name, path in config_data.items():
+        print(table_name, path)
+        # df = pd.read_parquet(path)
+        # df.to_sql(table_name, con=ENGINE)
